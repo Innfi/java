b52741c9070aee20d1e3565df42c5fdc59cc7f21
innfi <innfi.world@gmail.com>
2021-09-13 02:36:24 +0900

    mongodb: basic insert / find, index

--- playground/app/src/test/java/playground/AppTest.java
+++ playground/app/src/test/java/playground/AppTest.java
@@ -1,35 +1,15 @@
 package playground;
 
-import org.bson.Document;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import com.mongodb.client.MongoClients;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
-import com.mongodb.client.MongoClient;
-
 
 public class AppTest {
+
     @Test 
     public void appHasAGreeting() {
         App classUnderTest = new App();
         assertNotNull("app should have a greeting", classUnderTest.name());
     }
 
-    @Test 
-    public void mongodbConnect() {
-        MongoClient mongoClient = MongoClients.create("mongodb://localhost");
-
-        MongoDatabase db = mongoClient.getDatabase("testDB");
-        MongoCollection<Document> userCollection = db.getCollection("user");
-
-        List<Document> findResult = 
-            userCollection.find().into(new ArrayList<Document>());
-
-        assertEquals(findResult != null, true);
-    }
 }
---
+++ playground/app/src/test/java/playground/MongoDBTest.java
@@ -0,0 +1,80 @@
+package playground;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bson.Document;
+import org.bson.conversions.Bson;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.result.InsertOneResult;
+import com.mongodb.client.MongoClient;
+
+
+public class MongoDBTest {
+    public final String dbUrl = "mongodb://192.168.1.102";
+
+    //@Test 
+    public void insertAndFind() {
+        MongoClient mongoClient = MongoClients.create(dbUrl);
+
+        MongoDatabase db = mongoClient.getDatabase("testDB");
+        MongoCollection<Document> userCollection = db.getCollection("user");
+
+        Document inputDocument = new Document();
+        inputDocument.put("username", "innfi");
+        inputDocument.put("email", "innfi@test.com");
+
+        InsertOneResult result = userCollection.insertOne(inputDocument);
+        assertEquals(result.wasAcknowledged(), true);
+
+        Bson findFilter = Filters.eq("username", "innfi");
+        List<Document> findResult = 
+            userCollection.find(findFilter).into(new ArrayList<Document>());
+        assertEquals(findResult.get(0).get("username").toString(), "innfi");
+        assertEquals(findResult.get(0).get("email").toString(), "innfi@test.com");
+
+        //todo: teardown?
+        userCollection.drop();
+    }
+
+    @Test
+    public void setIndex() {
+        MongoClient mongoClient = MongoClients.create(dbUrl);
+
+        MongoDatabase db = mongoClient.getDatabase("testDB");
+        MongoCollection<Document> userCollection = db.getCollection("user");
+
+        String key = "username";
+        int order = 1;
+        userCollection.createIndex(Filters.eq(key, order));
+
+        dummyInsert(userCollection);
+
+        String indexName = key + "_" + order;
+        for (final Document doc: userCollection.listIndexes()) {
+            System.out.println("name: " + doc.getString("name").toString());
+
+            if(doc.getString("name").equals(indexName)) {
+                userCollection.drop();
+                return;
+            }
+        }
+
+        userCollection.drop();
+        fail();
+    }
+
+    protected void dummyInsert(MongoCollection<Document> userCollection) {
+        Document inputDocument = new Document();
+        inputDocument.put("username", "ennfi");
+        inputDocument.put("email", "ennfi@test.com");
+
+        userCollection.insertOne(inputDocument);
+    }
+}
