bfb167b437afbc594dc2e43bd7d563fcb89ff45d
innfi <innfi.world@gmail.com>
2021-09-16 19:23:57 +0900

    redis: hash, pubsub

--- playground/app/src/test/java/playground/RedisTest.java
+++ playground/app/src/test/java/playground/RedisTest.java
@@ -3,6 +3,9 @@ package playground;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import redis.clients.jedis.*;
 
 
@@ -27,5 +30,50 @@ public class RedisTest {
 
         String result = client.get("id").toString();
         assertEquals(result, testValue);
+    }
+
+    @Test
+    public void hashes() throws Exception {
+        JedisPool pool = new JedisPool(new JedisPoolConfig(), "localhost", 6379, 1000);
+        Jedis client = pool.getResource();
+
+        Map<String, String> hashValue = new HashMap<String, String>();
+        hashValue.put("name", "innfi");
+        hashValue.put("email", "innfi@test.com");
+        hashValue.put("message", "hello");
+        
+        client.hset("userProfile", hashValue);
+
+
+        String name = client.hget("userProfile", "name");
+        assertEquals(name, "innfi");
+    }
+
+    @Test 
+    public void pubsub() throws Exception {
+        JedisPool pool = new JedisPool(new JedisPoolConfig(), "localhost", 6379, 1000);
+        Jedis client = pool.getResource();
+
+        JedisPubSub pubsubClient = new JedisPubSub() {
+            @Override 
+            public void onMessage(String channel, String message) {
+                System.out.println("message: " + message);
+            }
+
+            @Override 
+            public void onSubscribe(String channel, int subscribedChannels) {
+
+            }
+
+            @Override 
+            public void onUnsubscribe(String channel, int subscribedChannels) {
+
+            }
+        };
+
+        String channel = "event";
+        client.subscribe(pubsubClient, channel);
+
+        client.publish(channel, "hello");
     }
 }
